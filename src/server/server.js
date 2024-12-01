dbdd85edf4de756c66b959d14741daacbf97a91e
Mzzqq <rezarifa661@gmail.com>
2024-12-02 03:41:09 +0700

    feat(server): menambahkan konfigurasi routing dan error handling global

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,6 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('../server/routes');
+
 (async () => {
     const server = Hapi.server({
         port: 3000,
@@ -8,7 +10,30 @@ const Hapi = require('@hapi/hapi');
                 origin: ['*'],
             },
         },
-    })
+    });
+
+    server.route(routes);
+    server.ext('onPreResponse', function (request, h) {
+        const response = request.response;
+        if (response instanceof InputError) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: `${response.message} Silakan gunakan foto lain.`
+            })
+            newResponse.code(response.statusCode)
+            return newResponse;
+        }
+        if (response.isBoom) {
+            const newResponse = h.response({
+                status: 'fail',
+                message: response.message
+            })
+            newResponse.code(response.statusCode)
+            return newResponse;
+        }
+        return h.continue;
+    });
+
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
